7b88e4f7bc9b32aa6bf9131f86829d105a7a1193
Olivier <olivierdenis99@hotmail.com>
2025-04-04 22:02:54 -0400

    Warden armor potion effects

---
+++ server_scripts/warden_armor.js
@@ -0,0 +1,36 @@
+// PlayerEvents.tick(event => {
+//     let p = event.player
+//     if (p.helmet == "rune_helmet" && p.body == "rune_body" && p.leggings == "rune_leggings" && p.boots == "rune_boots") {
+//     p.potionEffects.add("fire_resistance,100,1,true")
+//     }
+// })
+
+function checkArmorSet(player, prefix) {
+    return !player.armorSlots.some(a => 
+        !a.id.startsWith(prefix)
+    )
+};
+
+PlayerEvents.tick(event => {
+    const { player } = event;
+
+    // Fires event once a second
+    if (!(player.tickCount % 100 === 0)) {
+        return;
+    }
+    let prefix = "deeperdarker:warden_";
+    // Check if the player is wearing the full armor set
+    console.log(checkArmorSet(player, prefix));
+    if (checkArmorSet(player, prefix)) {
+        // Apply regeneration potion effect
+        let potion = event.player.potionEffects
+        for (let effect of [
+            'minecraft:strength',
+            'minecraft:resistance'
+            ]
+        ) {
+            potion.add(effect, 300, 1)
+        }
+
+    }
+});
